b96b8f5ca9804cd9eecb0d8d0fc08da22fb47a8e
Occidits <180444689+Occidits@users.noreply.github.com>
2026-03-11 19:26:22 +0100

    Create logic to get user choice

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+//variable to contain users choice
+let userChoice = prompt("Please, select R - Rock, P - Paper or S - Scissor");
+
 //Create function that returns the computer choice
 function getComputerChoice() {
   //Create an random variable that return a value between 0 and 33
   let randomNumber = Math.round(Math.round(Math.random() * 100) / 3);
-  console.log(randomNumber);
 
   //Conditional that compare randomNumber between a range to return 3 values
   if (randomNumber <= 11) {
     let rock = "Rock";
-    return console.log(rock);
+    return rock;
   } else if (randomNumber <= 22) {
     let paper = "Paper";
-    return console.log(paper);
+    return paper;
   } else {
     let scissor = "Scissor";
-    return console.log(scissor);
+    return scissor;
   }
 }
-console.log(getComputerChoice());
+
+//function that returns users choice
+function getUserChoice() {
+  //Variable to contain userChoice formatted to lowerCase
+  let choice = userChoice.toLowerCase();
+
+  //Conditional to return a value based in choice value
+  if (choice === "r") {
+    let userRock = "Rock";
+    return userRock;
+  } else if (choice === "p") {
+    let userPaper = "Paper";
+    return userPaper;
+  } else if (choice === "s") {
+    let userScissor = "Scissor";
+    return userScissor;
+  } else {
+    console.log("Please, refresh the page and select again");
+  }
+}
