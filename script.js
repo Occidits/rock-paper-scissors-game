837ebb3619aea2b3bb9cf80bf498dd24436113c6
Occidits <180444689+Occidits@users.noreply.github.com>
2026-03-11 20:39:17 +0100

    Create the logic to run the game and determines a winner

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//variable to contain users choice
-let userChoice = prompt("Please, select R - Rock, P - Paper or S - Scissor");
+//Variable to contain users choice
+let userChoice = prompt("Please, select Rock, Paper or Scissor");
+
+//Variables to track player and computer score
+let humanScore = 0;
+let computerScore = 0;
 
 //Create function that returns the computer choice
 function getComputerChoice() {
@@ -19,22 +23,48 @@ function getComputerChoice() {
   }
 }
 
-//function that returns users choice
-function getUserChoice() {
+//Function that returns users choice
+function getHumanChoice() {
   //Variable to contain userChoice formatted to lowerCase
   let choice = userChoice.toLowerCase();
 
   //Conditional to return a value based in choice value
-  if (choice === "r") {
+  if (choice === "rock") {
     let userRock = "Rock";
     return userRock;
-  } else if (choice === "p") {
+  } else if (choice === "paper") {
     let userPaper = "Paper";
     return userPaper;
-  } else if (choice === "s") {
+  } else if (choice === "scissor") {
     let userScissor = "Scissor";
     return userScissor;
   } else {
     console.log("Please, refresh the page and select again");
   }
 }
+
+//Function gets humanChoice and computerChoice
+//Function compares the two choices
+//Conditional to determine the winner, based in Rock, Paper scissor rules
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "Rock" && computerChoice === "Scissor") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissor" && computerChoice === "Paper")
+  ) {
+    humanScore++;
+    console.log("Human Wins!");
+  } else if (humanChoice === computerChoice) {
+    console.log("Draw!");
+  } else {
+    computerScore++;
+    console.log("Computer Wins!");
+  }
+}
+
+//Variables to use as arguments to playRound
+//they contain the result from getHumanChoice and getComputerChoice functions
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
